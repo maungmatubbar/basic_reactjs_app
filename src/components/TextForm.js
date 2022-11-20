521560b25824a4a3a22620669eeb95873c9c46b6
maungmatubbar <maungmatubbar@gmail.com>
2022-11-20 20:18:10 +0600

    Text count using .length, word count using slipt

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,6 +6,10 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText);
     }
+    const handleLoClick = () =>{
+      let newtext = text.toLocaleLowerCase();
+      setText(newtext);
+    }
     const handleOnChange = (event)=>{
         //console.log('On Changed');
         setText(event.target.value)
@@ -19,7 +23,15 @@ export default function TextForm(props) {
         </div>
         <div className="card-body">
             <textarea name="" id="convertText" value={text} onChange={handleOnChange} cols="30" rows="10" className='form-control'></textarea>
-            <button className='btn btn-primary mt-2' onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className='btn btn-primary mt-2 mx-1' onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className='btn btn-primary mt-2 mx-1' onClick={handleLoClick}>Convert to Lowercase</button>
+        </div>
+        <div className="card-footer">
+          <h2>Your text summary</h2>
+          <p>{text.split(" ").length} words and {text.length} characters</p>
+          <p>{0.008 * text.split(" ").length} minutes read</p>
+          <h2>Preview</h2>
+          <p className='cart-text'>{text}</p>
         </div>
       </div>
     </div>
